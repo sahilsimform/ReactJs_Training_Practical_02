23031ff88f3db34fad05fc685229ea22f92af523
sahilsimform <sahilsinghsimform@gmail.com>
2022-02-08 14:38:59 +0530

    Created React App by use of Webpack

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world! My name is Sahil Singh</h1>
+        <h3>I made this React App without command "create-react-app"</h3>
+        <p>I have use Webpack, babel.</p>
+      </div>
+    );
+  }
+}
+export default App;
